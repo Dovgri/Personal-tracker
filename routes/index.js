8289aa45f9ec185bfc1b0c13f104030afba1782c
Dovydas <dovydukasg@gmail.com>
2026-08-19 14:17:47 +0300

    Pridėjau naują puslapį "Knygos" su forma, kurioje galima įvesti knygos pavadinimą, autorių ir žanrą. Šį dar reik pabaigti, bet jau dabar galima įvesti duomenis ir jie bus atspausdinti konsolėje.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Main menu' });
+  res.render('index', { title: 'Pagrindinis puslapis' });
 });
 router.get('/training', function(req, res, next) {
-  res.render('training', { title: 'Training' });
+  res.render('training', { title: 'Sportas' });
+});
+router.get('/books', function(req, res, next) {
+  res.render('books', { title: 'Knygos' });
+});
+router.post('/books', function(req, res, next) {
+  const bookTitle = req.body.title;
+  const bookAuthor = req.body.author;
+  const bookGenre = req.body.genre;
+  console.log(`Book Title: ${bookTitle}, Author: ${bookAuthor}, Genre: ${bookGenre}`);
+  res.redirect('/books');
 });
 
 module.exports = router;
